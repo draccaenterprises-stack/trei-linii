861c0b9dab77d116e2472b3c02c9a687f89114f5
draccaenterprises-stack <dracca.enterprises@gmail.com>
2026-06-06 22:21:43 +0300

    fix: remove duplicate size table

--- src/routes/product.$handle.tsx
+++ src/routes/product.$handle.tsx
@@ -209,41 +209,6 @@ function ProductPage() {
                 </ul>
               </InfoBlock>
 
-              <InfoBlock title="Tabel marimi">
-                <div className="overflow-x-auto">
-                  <table className="w-full text-left font-mono-xs border border-border">
-                    <thead className="bg-cream">
-                      <tr>
-                        <th className="p-3 border-r border-border">Marime</th>
-                        <th className="p-3 border-r border-border">Piept</th>
-                        <th className="p-3 border-r border-border">Lungime</th>
-                        <th className="p-3">Umar</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {[
-                        ["S", "56 cm", "70 cm", "53 cm"],
-                        ["M", "59 cm", "72 cm", "55 cm"],
-                        ["L", "62 cm", "74 cm", "57 cm"],
-                        ["XL", "65 cm", "76 cm", "59 cm"],
-                      ].map((row) => (
-                        <tr key={row[0]} className="border-t border-border">
-                          {row.map((cell) => (
-                            <td key={cell} className="p-3 border-r border-border last:border-r-0">
-                              {cell}
-                            </td>
-                          ))}
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-                <p className="mt-3 text-muted-foreground">
-                  Daca esti intre doua marimi, alege marimea mai mare pentru un fit oversized mai
-                  vizibil.
-                </p>
-              </InfoBlock>
-
               <InfoBlock title="Ingrijire">
                 Spala pe dos la 30 grade C, nu folosi inalbitor si evita uscarea automata. Calca pe
                 dos, fara contact direct cu printul.
